26b8b9834cdcff5e68ea33416d8bf95817c832c3
ysfrdvn <yusufridvansavut@gmail.com>
2019-08-22 23:27:53 +0300

    global paket kullanım örnegi windows

---
+++ Global Module Use/app1/app1.js
@@ -0,0 +1,2 @@
+const l = require('C:/Users/ysfrdvn/AppData/Roaming/npm/node_modules/slugify');
+console.log(l("Hello. This is global module use"));
---
+++ Global Module Use/app2/app2.js
@@ -0,0 +1,2 @@
+const l = require('C:/Users/ysfrdvn/AppData/Roaming/npm/node_modules/slugify');
+console.log(l("hello world"));
